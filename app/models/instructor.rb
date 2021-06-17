38dd922de84e7421ce7f618676d3ca5402c9ab37
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-17 12:28:38 +0000

    lab done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,53 @@
 class Instructor
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def boating_tests
+        BoatingTest.all.select{|boating_test| boating_test.instructor == self}
+    end
+
+    def passed_boating_tests
+        boating_tests.select{|boating_test| boating_test.boating_test_status == "passed"}
+    end
+
+    def all_students
+        boating_tests.map{|boating_test| boating_test.student}
+    end
+
+    def passed_students
+        passed_boating_tests.map{|boating_test| boating_test.student}
+    end
+
+    def find_boating_test(student, boating_test_name)
+        boating_tests.find{|boating_test| boating_test.student == student && boating_test.boating_test_name == boating_test_name}
+    end
+
+    def pass_student(student, boating_test_name)
+        boating_test_found = find_boating_test(student, boating_test_name)
+        if boating_test_found
+            boating_test_found.boating_test_status = "passed"
+            return boating_test_found
+        else
+            return BoatingTest.new(student, boating_test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, boating_test_name)
+        boating_test_found = find_boating_test(student, boating_test_name)
+        if boating_test_found
+            boating_test_found.boating_test_status = "failed"
+            return boating_test_found
+        else
+            return BoatingTest.new(student, boating_test_name, "failed", self)
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,39 @@
 class Student
+    attr_reader :first_name
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tests
+        BoatingTest.all.select{|test| test.student == self}
+    end
+
+    def tests_passed
+        tests.select{|test| test.boating_test_status == "passed"}
+    end
+
+    def add_boating_test(boating_test_name, boating_test_status, instructor)
+        BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
+    end
+
+    def all_instructors
+        tests.map{|test| test.instructor}.uniq
+    end
+
+    def self.find_student(first_name)
+        self.all.find{|student| student.first_name == first_name}
+    end
+
+    def grade_percentage
+        tests_passed.count.to_f/tests.count * 100.0
+    end
+
 
 end
